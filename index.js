8551e779d51e67cd269241f52d4188779299aab2
mastero101 <mastershift_101@hotmail.com>
2024-05-04 03:47:21 -0600

    Change port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 5600;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
